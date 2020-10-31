be3042ee8002440e202cad5eb085615b1075908d
marizubia9 <m.zubia@opendeusto.es>
2020-10-31 18:22:18 +0100

    Conectando con BD

--- src/main/java/Aprendices_de_Josuka/LD/Categoria.java
+++ src/main/java/Aprendices_de_Josuka/LD/Categoria.java
@@ -1,8 +1,8 @@
 package Aprendices_de_Josuka.LD;
 
-import javax.jdo.annotations.PersistenceCapable;
 
-@PersistenceCapable
+
+
 public enum Categoria {
 
 	InfantilTxiki, InfantilHonor, CadeteHonor, CadeteeVasca, JuvenilVsaca, JuvenilNacional, JuvenilDivisionDeHonor
--- src/main/java/Aprendices_de_Josuka/LD/Tipo_Material.java
+++ src/main/java/Aprendices_de_Josuka/LD/Tipo_Material.java
@@ -2,7 +2,7 @@ package Aprendices_de_Josuka.LD;
 
 import javax.jdo.annotations.PersistenceCapable;
 
-@PersistenceCapable
+
 public enum Tipo_Material {
 
 	Balones, Petos, Conos,  Picas, Barreras, Vallas
