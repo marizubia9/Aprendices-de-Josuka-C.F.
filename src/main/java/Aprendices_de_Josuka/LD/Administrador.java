dc65cdb7e30eae391f3bc207539b5577dc354f36
maiderdorron <maider.dorronsoro@opendeusto.es>
2020-11-01 17:13:42 +0100

    administradorek irakurtzeko objetua

---
+++ src/main/java/Aprendices_de_Josuka/LD/Administrador.java
@@ -0,0 +1,33 @@
+package Aprendices_de_Josuka.LD;
+
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.PrimaryKey;
+
+@PersistenceCapable
+public class Administrador {
+
+	@PrimaryKey
+	private String email;
+	private String psw;
+	
+	public Administrador(String email, String psw) {
+		super();
+		this.email = email;
+		this.psw = psw;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPsw() {
+		return psw;
+	}
+	public void setPsw(String psw) {
+		this.psw = psw;
+	}
+	
+	
+	
+}
