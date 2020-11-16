a024a3ded5a878b224eddb8a81b4ef2078c84ff3
eideralbarracin <eider.a98@opendeusto.es>
2020-11-16 21:23:43 +0100

    Conseguir partidos y crear clasificacion #JAVADIVAS-42 add work 4h development

--- src/main/java/Aprendices_de_Josuka/LD/Partido.java
+++ src/main/java/Aprendices_de_Josuka/LD/Partido.java
@@ -1,6 +1,7 @@
 package Aprendices_de_Josuka.LD;
 
 import java.sql.Date;
+import java.time.LocalDateTime;
 import java.util.Set;
 
 import javax.jdo.annotations.Column;
@@ -16,23 +17,25 @@ public class Partido {
 
 	@PrimaryKey
 	private String cod_partido;
-	private Date fecha;
-	private int resultado;
+	private LocalDateTime fecha;
+	private long resultado_e1;
+	private long resultado_e2;
 	@Column(name = "equipo_1")
-	private Equipo equipo_1;
+	private Equipos_Ext equipo_1;
 	@Column(name = "equipo_2")
-	private String equipo_2;
+	private Equipos_Ext equipo_2;
 	private Set<Sancion> sanciones;
 	// private Equipo equipo_2;
 	// private Entrenador entrenador_sancionado_equipo1;
 	// private Entrenador entrenador_sancionado_equipo2;
 	// private Set<Jugador> jugador_sancionado;
 
-	public Partido(String cod_partido, Date fecha, int resultado, Equipo equipo_1, String equipo_2) {
+	public Partido(String cod_partido, LocalDateTime fecha, long resultado_e1, long resultado_e2, Equipos_Ext equipo_1, Equipos_Ext equipo_2) {
 		super();
 		this.cod_partido = cod_partido;
 		this.fecha = fecha;
-		this.resultado = resultado;
+		this.resultado_e1 = resultado_e1;
+		this.resultado_e2 = resultado_e2;
 		this.equipo_1 = equipo_1;
 		this.equipo_2 = equipo_2;
 		Sancion s= new Sancion();
--- src/main/java/Aprendices_de_Josuka/LN/Gestor.java
+++ src/main/java/Aprendices_de_Josuka/LN/Gestor.java
@@ -3,7 +3,11 @@ package Aprendices_de_Josuka.LN;
 import java.rmi.RemoteException;
 import java.sql.Date;
 import java.util.ArrayList;
+import java.util.Arrays;
+
 import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -11,13 +15,20 @@ import java.util.Set;
 import java.util.StringTokenizer;
 import java.util.stream.Collectors;
 
+import org.json.simple.parser.ParseException;
+
+import com.mysql.fabric.xmlrpc.base.Array;
+
 import Aprendices_de_Josuka.DAO.DAO;
+import Aprendices_de_Josuka.Gateway.Gateway;
 import Aprendices_de_Josuka.LD.Administrador;
 import Aprendices_de_Josuka.LD.Categoria;
 import Aprendices_de_Josuka.LD.Entrenador;
 import Aprendices_de_Josuka.LD.Equipo;
+import Aprendices_de_Josuka.LD.Equipos_Ext;
 import Aprendices_de_Josuka.LD.Jugador;
 import Aprendices_de_Josuka.LD.Material;
+import Aprendices_de_Josuka.LD.Partido;
 import Aprendices_de_Josuka.LD.Tipo_Material;
 import Aprendices_de_Josuka.LP.Pantalla_Principal;
 
@@ -289,5 +300,78 @@ public class Gestor {
 	public void ActualizarEquipo(Equipo equipo, HashMap inventario, List<Jugador> jugadores)
 	{
 		DAO.getInstance().ActualizarEquipo(equipo, inventario, jugadores);
+	}
+	
+	public List<Partido> getPartidos() throws ParseException{
+		List<Partido> lista = Gateway.getInstance().getPartidos();
+		HashSet <Equipos_Ext> equipos = Gateway.getInstance().getEquipos();
+		for(Equipos_Ext a: equipos)
+		{
+			System.out.println(a.getCod() + " " +a.getNombre() + " " +a.getCategoria() + " "+ a.getPuntuacion());
+		}
+		return lista;
+	}
+	
+	public void clasificacion() throws ParseException
+	{
+		HashSet <Equipos_Ext> equipos = Gateway.getInstance().getEquipos();
+		List<Equipos_Ext> list = new ArrayList<Equipos_Ext>(equipos);
+		Collections.sort(list, new Comparator<Equipos_Ext>() {
+			@Override
+			public int compare(Equipos_Ext p1, Equipos_Ext p2) {
+				return new Integer((int) p2.getPuntuacion()).compareTo(new Integer((int) p1.getPuntuacion()));
+			}
+
+		});
+		System.out.println("----------------------------------------------------");
+		System.out.println("CLASIFICACION ALEVIN");
+		for(Equipos_Ext b: list)
+		{
+			if(b.getCategoria().toUpperCase().equals("ALEVIN"))
+			{
+				System.out.println(b.getCod() + " " + b.getNombre() + " " + b.getPuntuacion());
+			}
+		}
+		System.out.println("----------------------------------------------------");
+		System.out.println("CLASIFICACION CADETE");
+		for(Equipos_Ext b: list)
+		{
+			if(b.getCategoria().toUpperCase().equals("CADETE"))
+			{
+				System.out.println(b.getCod() + " " + b.getNombre() + " " + b.getPuntuacion());
+			}
+		}
+		System.out.println("----------------------------------------------------");
+		System.out.println("CLASIFICACION INFANTIL");
+		for(Equipos_Ext b: list)
+		{
+
+			if(b.getCategoria().toUpperCase().equals("INFANTIL"))
+			{
+				System.out.println(b.getCod() + " " + b.getNombre() + " " + b.getPuntuacion());
+			}
+		}
+		System.out.println("----------------------------------------------------");
+		System.out.println("CLASIFICACION SENIOR");
+		for(Equipos_Ext b: list)
+		{
+
+			if(b.getCategoria().toUpperCase().equals("SENIOR"))
+			{
+				System.out.println(b.getCod() + " " + b.getNombre() + " " + b.getPuntuacion());
+			}
+		}
+		System.out.println("----------------------------------------------------");
+		System.out.println("CLASIFICACION JUVENIL");
+		for(Equipos_Ext b: list)
+		{
+
+			if(b.getCategoria().toUpperCase().equals("JUVENIL"))
+			{
+				System.out.println(b.getCod() + " " + b.getNombre() + " " + b.getPuntuacion());
+			}
+		}
+		
+		
 	}
 }
