90e20d3111446e4d2bd1d2656c6331b41cdf80bd
Maiderdorron <maider.dorronsoro@opendeusto.es>
2020-12-02 20:32:16 +0100

    testeo #JAVADIVAS-33 add work 1h development

---
+++ src/test/java/Aprendices_de_Josuka/Aprendices_de_Josuka/testGateway.java
@@ -0,0 +1,31 @@
+package Aprendices_de_Josuka.Aprendices_de_Josuka;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.json.simple.parser.ParseException;
+import org.junit.Before;
+import org.junit.Test;
+
+public class testGateway {
+	
+//	@Before
+//	public void inicializar() throws IOException
+//	{
+//		String[] cmd = {"python","launch_AprendicesDeJosuka_partidos.py","--host", "127.0.0.1", "--port", "5000"};
+//	    Runtime.getRuntime().exec(cmd);
+//	}
+	@Test
+	public void partidos() throws ParseException
+	{
+		assertTrue(Gateway.Gateway.getInstance().partidos());
+	}
+	@Test
+	public void sanciones() throws ParseException
+	{
+		assertTrue(Gateway.Gateway.getInstance().sanciones());
+	}
+}
